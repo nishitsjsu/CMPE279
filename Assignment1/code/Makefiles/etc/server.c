b1a41202276e921f695a4df957a5f43c760ae6da
Nishit <nishitajaykumar.doshi@sjsu.edu>
2020-03-22 16:25:46 -0700

    Changed server code for privilege separation

--- Assignment1/code/Makefiles/etc/server.c
+++ Assignment1/code/Makefiles/etc/server.c
@@ -5,6 +5,8 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
+#include <sys/wait.h>
+#include <sys/user.h>
 #define PORT 8080 
 int main(int argc, char const *argv[]) 
 { 
@@ -14,6 +16,8 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address); 
     char buffer[1024] = {0}; 
     char *hello = "Hello from server"; 
+    int pid;
+    int uid;
        
     // Creating socket file descriptor 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
@@ -51,9 +55,33 @@ int main(int argc, char const *argv[])
         perror("accept"); 
         exit(EXIT_FAILURE); 
     } 
-    valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
+
+    printf("PID before fork : %d\n", getpid()); // Parent process
+    pid = fork(); // Creating child process to handle data coming from the client
+    printf("PID after fork : %d\n", pid);
+    printf("uid is : %d\n" , getuid());	// Child process
+    if(pid == 0){
+    	printf("child process %d is created and control is with child now\n", pid);
+    	uid = setuid(65534); // set user to "nobody - 65534" user
+    	printf("Uid: %d and user id is %d \n", uid, getuid()); // new userid confirmation
+    	if(uid == -1){
+    		// if setuid() throws error by returning -1 code then terminate the program
+    		printf("Error in setting the user to nobody user while dropping privileges\n");
+    		exit(EXIT_FAILURE);
+    	}
+    	valread = read( new_socket , buffer, 1024);
+	    printf("%s\n",buffer );  
+	    // kill(pid, SIGKILL);
+    } else if(pid > 0){
+    	sleep(2);
+    	printf("control is with parent now\n");
+	    send(new_socket , hello , strlen(hello) , 0 ); 
+		printf("Hello message sent\n");
+    } else{
+    	printf("Fork failed\n");
+    	exit(EXIT_FAILURE);
+    }
+    printf(" uid is : %d\n " , getuid());
     return 0; 
 } 
+
